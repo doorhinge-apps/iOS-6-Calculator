a77ca5364562863a32091d00175245cf495ed7ae
Caedmon Myers <caedmonmyers@icloud.com>
2025-05-29 14:58:31 -0500

    Initial Commit

---
+++ iOS 6 Calculator/ContentView.swift
@@ -0,0 +1,27 @@
+//
+// iOS 6 Calculator
+// ContentView.swift
+//
+// Created on 29/5/25
+//
+// Copyright ©2025 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS 6 Calculator/iOS_6_CalculatorApp.swift
@@ -0,0 +1,20 @@
+//
+// iOS 6 Calculator
+// iOS_6_CalculatorApp.swift
+//
+// Created on 29/5/25
+//
+// Copyright ©2025 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+@main
+struct iOS_6_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
